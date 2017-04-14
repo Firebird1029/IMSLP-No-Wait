6a3e2e20bb9660c5398461cf102284fedec75a92
Brandon Yee <byee20@punahou.edu>
2017-04-14 12:52:32 -1000

    Auto Reload
    Reloads page into PDF file instead of having user click on link.

--- removeWait.js
+++ removeWait.js
@@ -2,3 +2,6 @@ console.log("Removing wait time using IMSLP No-Wait extension!");
 
 var pdfUrl = $("#sm_dl_wait").attr("data-id");
 $("#sm_dl_wait").after(`<br><span>IMSLP No-Wait: <a href="${pdfUrl}">Click here</a>.</span>`);
+
+// Faster Method - Auto Reload
+window.location.href = pdfUrl;
