7e8f0f3ad83686865e52bb435728281c9e985a0e
Brandon <6111102+Firebird1029@users.noreply.github.com>
2020-09-07 17:09:03 -1000

    Fixed up an old project :')

--- removeWait.js
+++ removeWait.js
@@ -1,7 +1,9 @@
-console.log("Removing wait time using IMSLP No-Wait extension!");
+if ($("#sm_dl_wait").length) {
+	console.log("Removing wait time using IMSLP No-Wait extension!");
 
-var pdfUrl = $("#sm_dl_wait").attr("data-id");
-$("#sm_dl_wait").after(`<br><span>IMSLP No-Wait: <a href="${pdfUrl}">Click here</a>.</span>`);
+	var pdfUrl = $("#sm_dl_wait").attr("data-id");
+	$("#sm_dl_wait").after(`<br><span>IMSLP No-Wait: <a href="${pdfUrl}">Click here</a>.</span>`);
 
-// Faster Method - Auto Reload
-window.location.href = pdfUrl;
+	// Faster Method - Auto Reload
+	window.location.href = pdfUrl;
+}
